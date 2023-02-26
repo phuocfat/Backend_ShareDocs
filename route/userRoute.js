85847b1d10bc372d1fd8b3d6c4e9d522ca77a4f8
Phuoc Tang <phuocst24@gmail.com>
2023-02-26 12:38:10 +0700

    Update system file

--- route/userRoute.js
+++ route/userRoute.js
@@ -1,6 +1,17 @@
 const express = require("express");
-const { register, login } = require("../controller/userController");
 const router = express.Router();
+const { register, login } = require("../controller/userController");
+const authController = require("../controller/auth/auth.controllers");
+const {
+  sendLink,
+  connectRestPassword,
+  resetPassWord,
+} = require("../controller/forgotpassword");
+const refreshToken = authController.refreshToken;
 router.post("/register", register);
 router.post("/login", login);
+router.post("/forgotpassword", sendLink);
+router.get("/reset-password/:id/:token", connectRestPassword);
+router.post("/reset-password/:id/:token", resetPassWord);
+router.post("/refresh", refreshToken);
 module.exports = router;
